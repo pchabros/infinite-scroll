cabdf22b21102bbdedf5143bcd9ef91186a938c5
pchabros <pawel.chabros@yahoo.pl>
2022-07-11 22:44:54 +0200

    `max-width` added to `InfiniteList`

--- src/components/InfiniteList.tsx
+++ src/components/InfiniteList.tsx
@@ -8,7 +8,13 @@ interface InfiniteListProps<T> {
 const InfiniteList = <T,>({ data, renderItem }: InfiniteListProps<T>) => {
   return (
     <div
-      style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        maxWidth: 1200,
+        margin: "auto",
+      }}
     >
       {data.map((d) => renderItem(d))}
     </div>
